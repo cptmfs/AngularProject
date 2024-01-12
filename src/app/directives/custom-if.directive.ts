950d256c07f2a5a51bc8cc066b46ebe094497edf
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2024-01-13 01:48:06 +0300

    Değişikliklerinizi açıklama

---
+++ src/app/directives/custom-if.directive.ts
@@ -0,0 +1,22 @@
+import { Directive,Input,TemplateRef,ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appCustomIf]'
+})
+export class CustomIfDirective {
+
+  constructor(
+    private templateRef:TemplateRef<any>,
+    private viewContainerRef:ViewContainerRef
+  ) { }
+@Input() set appCustomIf(value:boolean)
+{
+  if (value) {
+    this.viewContainerRef.createEmbeddedView(this.templateRef)
+  }
+  else
+  {
+    this.viewContainerRef.clear()
+  }
+}
+}
